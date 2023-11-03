bb8f175cae40469d2dfa619efb5e818afa31a91d
alikapllan <alihkaplan44@gmail.com>
2023-11-03 09:45:56 +0100

    checkbox in table lines & moving data to 2nd table

--- uimodule/webapp/controller/HuSelection.controller.js
+++ uimodule/webapp/controller/HuSelection.controller.js
@@ -11,6 +11,37 @@ sap.ui.define([
         onNavToHuEdit(){
             const oRouter = this.getOwnerComponent().getRouter();
             oRouter.navTo("huEdit");
+        },
+
+        onDataToSecondTable(){
+            const oFirstTable = this.byId("firstTable");
+            const oSecondTable = this.byId("secondTable");
+            const aSelectedItems = oFirstTable.getSelectedItems();
+
+            aSelectedItems.forEach((oSelectedItem) => {
+                const oCells = oSelectedItem.getCells();
+                const sName = oCells[0].getText();
+                const sSurname = oCells[1].getText(); 
+                const sDescription = oCells[2].getText(); 
+
+                // new item to be added to second table
+                const oNewItem = new sap.m.ColumnListItem({
+                    cells: [
+                        new sap.m.Text({ text: sName }),
+                        new sap.m.Text({ text: sSurname }),
+                        new sap.m.Text({ text: sDescription })
+                    ]
+                });
+
+                // item to second table
+                oSecondTable.addItem(oNewItem);
+
+                // remove selected item from first table
+                oFirstTable.removeItem(oSelectedItem);
+            });
+
+            // clear selection from first table
+            oFirstTable.removeSelections();
         }
     });
 });

9c636fc664cc011b9e1f9abc947844bdf58eaef0
alikapllan <alihkaplan44@gmail.com>
2023-11-09 15:14:06 +0100

    Navigating Back

--- uimodule/webapp/controller/HuEdit.controller.js
+++ uimodule/webapp/controller/HuEdit.controller.js
@@ -1,11 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller)=>{
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
+], (Controller, History)=>{
     "use strict";
 
     return Controller.extend("com.myorg.humonitor.controller.HuEdit",{
         onInit(){
 
+        },
+
+        onNavBack(){
+            const oHistory = History.getInstance();
+            const sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                const oRouter = this.getOwnerComponent().getRouter();
+                oRouter.navTo("HuSelection", {}, true);
+            }
         }
     });
 });
